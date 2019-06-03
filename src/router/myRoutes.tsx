8b0206a058c2e1e3db2a4ec4f168f8f417a35897
Rafael Pelle <rafapelle@gmail.com>
2019-06-03 18:23:13 +0000

    Refactoring React lazy

--- src/router/myRoutes.tsx
+++ src/router/myRoutes.tsx
@@ -1,27 +1,16 @@
 import * as React from 'react'
 import { Redirect, Route } from 'react-router'
 import { iceGrey, pageHeaderHeight } from '../utils/theme'
-import LazyImport from '../components/lazyImport/lazyImport'
-// import HomePage from '../pages/homePage/homePage'
-// import LoginPage from '../pages/loginPage/loginPage'
 
-// const HomePage = LazyImport({
-// 	loader: () => import('../pages/homePage/homePage' /* webpackChunkName: 'HomePage' */),
-// })
-// const LoginPage = LazyImport({
-// 	loader: () => import('../pages/loginPage/loginPage' /* webpackChunkName: 'LoginPage' */),
-// })
+const Suspense = React.Suspense
 const HomePage = React.lazy(() => import('../pages/homePage/homePage'))
 const LoginPage = React.lazy(() => import('../pages/loginPage/loginPage'))
 
-const Suspense = React.Suspense
 
 const MyRoutes: React.FC<Props> = (props) => {
 	return (
 		<div style={ divStyle }>
-			<Suspense fallback={ <div>Loading...</div> }>
-				{ /*<Route exact={ true } path='/' component={ HomePage } />*/ }
-				{ /*<Route exact={ true } path='/login' component={ LoginPage } />*/ }
+			<Suspense fallback={ <div/> }>
 				<Route exact={ true } path='/' render={ () => <HomePage /> } />
 				<Route exact={ true } path='/home' render={ () => <Redirect to='/' /> } />
 				<Route exact={ true } path='/login' render={ () => <LoginPage /> } />
